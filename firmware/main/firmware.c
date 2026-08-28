cf1e0e4cd822c37643c2189b0da67afed91bbf17
adrianrosu54 <adrianrosu5400@gmail.com>
2026-08-29 01:26:27 +0300

    feat: ADC battery reading

--- firmware/main/firmware.c
+++ firmware/main/firmware.c
@@ -2,16 +2,23 @@
 #include <string.h>
 
 #include "bmp280.h"
+#include "esp_adc/adc_oneshot.h"
 #include "esp_err.h"
 #include "esp_log.h"
 #include "esp_sleep.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/idf_additions.h"
 #include "freertos/projdefs.h"
+#include "hal/adc_types.h"
 #include "i2cdev.h"
 
 #define I2C_MASTER_SDA 21
 #define I2C_MaSTER_SCL 22
+
+#define ADC_UNIT ADC_UNIT_2
+#define ADC_CHANNEL ADC_CHANNEL_0
+
+#define WAKEUP_TIME_SEC 10
 
 static const char *TAG = "SolarNode main";
 
@@ -19,7 +26,28 @@ typedef struct {
     float temperature;
     float pressure;
     float humidity;
+    int battery_voltage;
 } SolarNode_Data;
+
+static void sensorRead_battery(SolarNode_Data *restrict data) {
+    adc_oneshot_unit_handle_t adc_handle;
+    adc_oneshot_unit_init_cfg_t init_config = {.unit_id = ADC_UNIT};
+
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config, &adc_handle));
+
+    adc_oneshot_chan_cfg_t config = {.bitwidth = ADC_BITWIDTH_DEFAULT,
+                                     .atten = ADC_ATTEN_DB_12};
+
+    ESP_ERROR_CHECK(
+        adc_oneshot_config_channel(adc_handle, ADC_CHANNEL, &config));
+
+    vTaskDelay(pdMS_TO_TICKS(100));
+
+    ESP_ERROR_CHECK(
+        adc_oneshot_read(adc_handle, ADC_CHANNEL, &data->battery_voltage));
+
+    printf("ADC raw value: %d\n", data->battery_voltage);
+}
 
 static void sensorRead_bmp280(SolarNode_Data *restrict data) {
     ESP_ERROR_CHECK(i2cdev_init());
@@ -33,10 +61,10 @@ static void sensorRead_bmp280(SolarNode_Data *restrict data) {
     ESP_ERROR_CHECK(bmp280_init_desc(&dev, BMP280_I2C_ADDRESS_0, 0,
                                      I2C_MASTER_SDA, I2C_MaSTER_SCL));
     ESP_ERROR_CHECK(bmp280_init(&dev, &params));
-
-    ESP_LOGI(TAG, "BME280: found");
+    // ESP_LOGI(TAG, "BME280: found");
 
     vTaskDelay(pdMS_TO_TICKS(90));
+
     if (bmp280_read_float(&dev, &data->temperature, &data->pressure,
                           &data->humidity) != ESP_OK) {
         ESP_LOGW(TAG, "Temperature/pressure reading failed\n");
@@ -64,6 +92,7 @@ void app_main(void) {
     }
 
     sensorRead_bmp280(&data);
+    sensorRead_battery(&data);
 
     esp_sleep_enable_timer_wakeup(WAKEUP_TIME_SEC * 1000000ULL);
 
