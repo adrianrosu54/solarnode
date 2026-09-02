79ffdd72f899f36b59fc8ef0c201b4bbeb71c43c
adrianrosu54 <adrianrosu5400@gmail.com>
2026-09-02 22:48:17 +0300

    feat: go /data POST request handling

--- server/main.go
+++ server/main.go
@@ -6,6 +6,36 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type SolarNodeReading struct {
+	Temperature    float64 `json:"temperature"`
+	Pressure       float64 `json:"pressure"`
+	Humidity       float64 `json:"humidity"`
+	BatteryVoltage float64 `json:"battery_voltage"`
+}
+
+var (
+	temperature = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "solarnode_temperature_celsius",
+		Help: "Sensor temperature measured (BME280)",
+	})
+	pressure = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "solarnode_pressure_pascals",
+		Help: "Atmospheric temperature measured (BME280)",
+	})
+	humidity = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "solarnode_humidity_relative",
+		Help: "Relative humidity measured (BME280)",
+	})
+	batteryVoltage = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "solarnode_battery_voltage_volts",
+		Help: "Battery supply voltage (voltage divider ADC reading)",
+	})
+	storageMutex sync.RWMutex
 )
 
 func main() {
@@ -39,14 +69,43 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewEncoder(w).Encode(map[string]string{"status": "alive"})
 	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		log.Printf("Encoder error: %v\n", err)
+		return
 	}
 }
 
 func dataHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Processing sensor data...\n")
+	if r.Method != "POST" {
+		http.NotFound(w, r)
+		return
+	}
+
+	var newData SolarNodeReading
+
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+
+	err := decoder.Decode(&newData)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	log.Printf("/data\tNew data recieved: %+v\n", newData)
+
+	// update data
+	storageMutex.Lock()
+
+	temperature.Set(newData.Temperature)
+	pressure.Set(newData.Pressure)
+	humidity.Set(newData.Humidity)
+	batteryVoltage.Set(newData.BatteryVoltage)
+
+	storageMutex.Unlock()
+
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func metricsHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Prom metrics...\n")
 }
